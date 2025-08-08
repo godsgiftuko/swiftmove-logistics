6d1bd18d21c109c0d7776f5c03910d80b6dd4e9c
God'sgift Uko <godsgiftuko@gmail.com>
2025-08-08 23:10:44 +0100

    Update DriverService.findById parameter type

--- packages/server/src/services/driver.service.ts
+++ packages/server/src/services/driver.service.ts
@@ -1,6 +1,7 @@
 import { HTTP_STATUS } from "@/constants";
 import User, { EUserRole, EUserStatus, IUser } from "../models/user.model";
 import { ServiceResponse } from "../types/service";
+import mongoose from "mongoose";
 
 export class DriverService {
   //  List active drivers
@@ -11,5 +12,12 @@ export class DriverService {
     }
     const drivers = await User.find(conditions);
     return [drivers, null, HTTP_STATUS.OK];
+  }
+
+  //  Find driver by id
+  static async findById(id: mongoose.Types.ObjectId): Promise<ServiceResponse<IUser>> {
+    const user = await User.findOne({ _id: id, role: EUserRole.driver });
+    if (!user) return [null, 'Driver not found', HTTP_STATUS.NOT_FOUND];
+    return [user, null, HTTP_STATUS.OK];
   }
 }
