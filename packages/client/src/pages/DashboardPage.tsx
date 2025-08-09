331a6e39561936f6855ac2d6ab826c36f26eaad6
God'sgift Uko <godsgiftuko@gmail.com>
2025-08-09 04:18:51 +0100

    Setup tailwind

--- packages/client/src/pages/DashboardPage.tsx
+++ packages/client/src/pages/DashboardPage.tsx
@@ -1,5 +1,5 @@
 import { API } from '../../../shared/constants';
 
 export default function DashboardPage() {
-    return <h1>{API.NAME} DashboardPage</h1>
+    return <h1 className='text-4xl font-bold'>{API.NAME} DashboardPage</h1>
 }
