a30f9134d9624031bc4c38ca050a995658658eb2
God'sgift Uko <godsgiftuko@gmail.com>
2025-08-10 19:25:48 +0100

    Add new EWSEvents events

--- packages/shared/enums/ws.ts
+++ packages/shared/enums/ws.ts
@@ -1,3 +1,9 @@
 export enum EWSEvents {
     USER_CONNECTED = 'user.connected',
+    USER_DISCONNECTED = 'user.disconnected',
+    DELIVERY_ASSIGNED = 'delivery.assigned',
+    DELIVERY_DELIVERED = 'delivery.delivered',
+    DELIVERY_PENDING = 'delivery.pending',
+    DELIVERY_CANCELLED= 'delivery.cancelled',
+    DELIVERY_IN_TRANSIT= 'delivery.intransit',
 }
