10e498cc3c311c0a855081cede07151abb83610a
God'sgift Uko <godsgiftuko@gmail.com>
2025-08-12 16:37:16 +0100

    try hosting src/index.ts

--- packages/server/api/index.ts
+++ packages/server/api/index.ts
@@ -1,24 +1,24 @@
-// import serverless from "serverless-http";
-// import app from "../src/server";
+import serverless from "serverless-http";
+import app from "../src";
 
-// export const handler = serverless(app);
-import express from 'express';
-import serverless from 'serverless-http';
-
-const app = express();
-
-// Middleware
-app.use(express.json());
-
-// Routes
-app.get('/', (req, res) => {
-  res.send('Hello from Express on Vercel!');
-});
-
-app.get('/api/hello', (req, res) => {
-  res.json({ message: 'Hello API!' });
-});
-
-// Export as serverless function
-export default app;
 export const handler = serverless(app);
+// import express from 'express';
+// import serverless from 'serverless-http';
+
+// const app = express();
+
+// // Middleware
+// app.use(express.json());
+
+// // Routes
+// app.get('/', (req, res) => {
+//   res.send('Hello from Express on Vercel!');
+// });
+
+// app.get('/api/hello', (req, res) => {
+//   res.json({ message: 'Hello API!' });
+// });
+
+// // Export as serverless function
+// export default app;
+// export const handler = serverless(app);
