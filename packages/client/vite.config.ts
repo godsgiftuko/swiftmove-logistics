0a7d64d56fa95be2e416e5996e025eb486e8459a
God'sgift Uko <godsgiftuko@gmail.com>
2025-08-12 17:39:50 +0100

    comment client proxy

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -1,20 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import { API } from '../shared/constants'
+// import { API } from '../shared/constants'
 // https://vitejs.dev/config/
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
     port: 3000,
-    proxy: {
-      '/api': {
-        target: API.BASE_URL,
-        changeOrigin: true,
-        secure: false,
-      }
-    }
+    // proxy: {
+    //   '/api': {
+    //     target: API.BASE_URL,
+    //     changeOrigin: true,
+    //     secure: false,
+    //   }
+    // }
   },
   build: {
     outDir: 'dist',
